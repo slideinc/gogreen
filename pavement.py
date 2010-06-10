b4918347ba81a16c76d50b414748587148fbb012
Travis J Parker <travis.parker@gmail.com>
2010-06-10 16:01:00 -0700

    license, distribution, .gitignore, put the source in a real package

---
+++ pavement.py
@@ -0,0 +1,58 @@
+import errno
+import os
+from setuptools import Extension
+
+from paver.easy import *
+from paver.path import path
+from paver.setuputils import setup
+
+
+setup(
+    name="gogreen",
+    description="",
+    version="1.0",
+    license="bsd",
+    packages=["gogreen"],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: C",
+        "Topic :: System :: Networking",
+    ]
+)
+
+MANIFEST = (
+    "LICENSE",
+    "setup.py",
+    "paver-minilib.zip",
+    "itimermodule.c",
+)
+
+@task
+def manifest():
+    path('MANIFEST.in').write_lines('include %s' % x for x in MANIFEST)
+
+@task
+@needs('generate_setup', 'minilib', 'manifest', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('itimer.egg-info', 'dist', 'build', 'MANIFEST.in')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
+    for p in path(__file__).abspath().parent.walkfiles():
+        if p.endswith(".pyc") or p.endswith(".pyo"):
+            try:
+                p.remove()
+            except OSError, exc:
+                if exc.args[0] == errno.EACCES:
+                    continue
+                raise
