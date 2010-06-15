2a631af9f542887e70476ecbdc1db19654a52a58
Travis J Parker <travis.parker@gmail.com>
2010-06-15 10:43:49 -0700

    bump version for a clean release

--- pavement.py
+++ pavement.py
@@ -10,7 +10,7 @@ from paver.setuputils import setup
 setup(
     name="gogreen",
     description="Coroutine utilities for non-blocking I/O with greenlet",
-    version="1.0",
+    version="1.0.1",
     license="bsd",
     author="Libor Michalek",
     author_email="libor@pobox.com",
