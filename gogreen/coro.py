b4587058068b6b224e020d5b30ef29b20b0ed64a
Travis J Parker <travis.parker@gmail.com>
2010-09-22 16:15:09 -0700

    unused imports (bisect!)

--- gogreen/coro.py
+++ gogreen/coro.py
@@ -30,16 +30,13 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import pdb
 import traceback
-import bisect
 import os
 import select
 import string
 import signal
 import sys
 import time
-import random as whrandom
 import logging
 import errno
 import weakref
